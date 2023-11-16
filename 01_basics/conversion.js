07bb50ecd0abc2bdfede4ea762039d297eb326d5
ANURAG RISHI <111632025+anurag2907@users.noreply.github.com>
2023-11-16 06:38:17 +0000

    operation performed

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -11,9 +11,53 @@
 //true=>1; false=>0
 
 //" "= false
-//"hitesh"= true 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//"hitesh"= true
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//***********************operations******************** */
+
+//  let value =3
+//  let negValue=-value
+ //console.log(negValue)
+
+//   console.log(2+1);
+//   console.log(2-2);
+//   console.log(2*2);
+//   console.log(2**3);
+//   console.log(2/3);
+//   console.log(2%3);
+
+
+  let srt1 = "pug";
+  let str2 = " kumar";
+  let str3 = str1+str2;
+
+
+
+
+//("1"+2+2)=>122
+//(1+2+"2")=>32
+
+//prefix or postfix ko humlog ko mdn se dekh kena ha 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
