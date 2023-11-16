ba59b65d8170a85a55c0eb9c741d9cb8f6468fb3
ANURAG RISHI <111632025+anurag2907@users.noreply.github.com>
2023-11-16 05:43:17 +0000

    yea data type ka example tha

---
+++ 01_basics/datatype.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as a newear version
+//alert(3+3)//use are usin node js node browser 
+console.log(3+3)  
+//tc39 os original documentation hum log ko liye mdn padhna pade ga 
+
+//type of datatype
+//number =>2 to power 53
+//bigint
+//string 
+//boolean=>true/false
+//null=>stanalone value
+//undefined
+//symbol=>unique
+
+//object
+
+let name="anurag"
+console.log(typeof "anurag")
