3f6c5e46235466dd6fe62b08c02e5f4b32d79372
ANURAG RISHI <111632025+anurag2907@users.noreply.github.com>
2023-11-16 06:19:10 +0000

    conversion of data types

---
+++ 01_basics/conversion.js
@@ -0,0 +1,19 @@
+// let score="33"
+
+// //let score="33abc" agar aysa hota to isko v convert kar deta but jab isko print karwate to  not a nan(not a number deta)
+// let  valueInNumber= Number(score)
+// //score ka value null hota to usko number me 0 me covert kar deta 
+// console.log(typeof valueInNumber);
+// console.log(typeof score);
+// console.log(valueInNumber);
+//"33"=>33(string ko number me convert karne par)
+//"33abc"=>NaN
+//true=>1; false=>0
+
+//" "= false
+//"hitesh"= true 
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
