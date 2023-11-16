252630f7f7a4468f1207bc5f0232670654cff4e3
ANURAG RISHI <111632025+anurag2907@users.noreply.github.com>
2023-11-16 05:29:06 +0000

    new updata

---
+++ 01_basics/variable.js
@@ -0,0 +1,14 @@
+const accountId=12345
+let accountEmail="anuragrishi29072@gmail.com"
+var accountPassward ="123456"
+//prefre not to use var bcz it is not a lock scope and function scope
+accountCity ="Dhanbad";
+let accountState;
+
+//accountId =2 agra may liku ga to may isko change nahi kar pau ga 
+//const key se ja bante ha usko change nahi kar pay ge 
+// not allowed
+
+
+console.table([accountId,accountEmail,accountPassward,accountCity])//agar table ke jays print karwana ho to ayse kare the print 
+
