4857495bec964910dc889bf707c1257d985c5213
ANURAG RISHI <111632025+anurag2907@users.noreply.github.com>
2023-11-16 06:47:54 +0000

    comparision dekhan tha isme

---
+++ 01_basics/comprision.js
@@ -0,0 +1,10 @@
+console.log(2>10)
+console.log(2>=1)
+//is tarha ka comprision true or false me data deta ha
+//do alag datatype ko compare kare ge to dikkat deta ha yea man mutabiat ke hisab se answer  nahi deta ha  
+console.log(undefined==0)//yea true dega 
+console.log(undefined>0)//yea true dega 
+console.log(undefined<0)//yea true dega 
+
+//uppar wala type ka coversion avoide karna ha humlogo ko
+
